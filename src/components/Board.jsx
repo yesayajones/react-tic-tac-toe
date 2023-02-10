476fa32618ba30694724b1de0d98ff8041e963af
yesayajones <yesayajones@gmail.com>
2023-02-10 14:04:37 +0200

    fix: Remove gitignore files

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Board() {
 	return (
 		<div className='board' id='board'>
-			//create nine cells
 			<div className='cell' data-cell></div>
 			<div className='cell' data-cell></div>
 			<div className='cell' data-cell></div>
--- src/components/WinningMessage.jsx
+++ src/components/WinningMessage.jsx
@@ -4,7 +4,6 @@ function WinningMessage() {
 	return (
 		<div class='winning-message' id='winningMessage'>
 			<div data-winning-message-text></div>
-			//Restart game button
 			<button id='restartButton'>Restart</button>
 		</div>
 	);
